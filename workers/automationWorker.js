db2b6b32c0d9b35aee17a3358e1dbe2d52c67285
muhammadali099 <muhammadaliamir177@gmail.com>
2025-12-18 18:27:25 +0500

    remove the username and password in redis connection

--- workers/automationWorker.js
+++ workers/automationWorker.js
@@ -360,8 +360,6 @@ function getModelByName(modelName) {
 const connection = {
   host: process.env.REDIS_HOST || "127.0.0.1",
   port: Number(process.env.REDIS_PORT || 6379),
-  username: process.env.REDIS_USERNAME,
-  password: process.env.REDIS_PASSWORD, // ✅ THIS FIXES NOAUTH
 };
 
 const worker = new Worker(
