455d1480e8f372155e71de5f35443ef881b45c8e
mashhoodrehman <mashhoodr.rehman@gmail.com>
2026-03-17 23:01:55 +0500

    Fixedwhatsapp message

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -50,7 +50,7 @@ const processSetupFee = async (req, res) => {
     // Indicator URL for webhook
     const indicatorUrl = `${process.env.BASE_URL}/api/cardcom/indicator`;
     const successUrl = `https://www.invitenow-ai.com/onboarding?step=6&eventId=${eventId}`;
-    const errorUrl = `https://www.invitenow-ai.com//payment-error?eventId=${eventId}`;
+    const errorUrl = `https://www.invitenow-ai.com/payment-error?eventId=${eventId}`;
 
     // Create Cardcom Payment Link
     const redirectUrl = await CardcomService.createPaymentLink({
